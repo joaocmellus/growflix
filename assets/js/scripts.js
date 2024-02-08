e9f3ab890e8edb73bfe1c65ae759c136f7b8854b
João Caetano de Mello <joaomellus0@gmail.com>
2024-02-08 00:12:46 -0300

    feat: Add video modal and interactions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+(() => {
+const modal = new bootstrap.Modal('#modal-fullscreen');
 const categories = {
     'growcast' : movies.filter((movie) => movie.category == 'growcast'),
     'webinar'  : movies.filter((movie) => movie.category == 'webinar'),
@@ -22,6 +24,43 @@ function renderMovieCard(title, image, link) {
     </div>`
 }
 
+function renderIframe(link) {
+    return `
+    <div class="ratio ratio-21x9">
+        <iframe class="container-fluid"
+            width="100%" 
+            height="100%" 
+            src="${link}" 
+            title="YouTube video player" frameborder="0" 
+            allow="
+                accelerometer;
+                autoplay;
+                clipboard-write;
+                encrypted-media;
+                gyroscope;
+                picture-in-picture;
+                web-share
+            "
+            allowfullscreen
+            onload="document.querySelector('#loading').style.display = 'none';">
+        </iframe>
+        <div id="loading" class="spinner-border top-50 start-50" role="status" style="height:50px;width:50px">
+            <span class="visually-hidden">Carregando vídeo...</span>
+        </div>
+    </div>`
+}
+
+function updateModal(title, link) {
+    const modalRef = modal._element;
+    const modalTitle = modalRef.querySelector('.modal-title');
+    const modalBody = modalRef.querySelector('.modal-body');
+    
+    modalTitle.innerText = title;
+    modalBody.innerHTML = renderIframe(link);
+
+    modal.show();
+}
+
 function setup() {
     for (const category in categories) {
         const ref = document.getElementById(category).parentElement.parentElement;
@@ -30,6 +69,23 @@ function setup() {
             ref.innerHTML += renderMovieCard(movie.title, movie.img, movie.link);
         });
     }
+
+    document.querySelectorAll('.movie-link').forEach( element => {
+        element.addEventListener('click', (e) => {
+            let element = e.target;
+            while (!element.dataset.link) {
+                element = element.parentElement;
+            }
+            const link = element.dataset.link;
+            const title = element.querySelector('h3').innerText;
+            updateModal(title, link);
+        });
+    });
+
+    document.querySelector('#close-modal').addEventListener('click', (e) => {
+        modal._element.querySelector('.modal-body').innerHTML='';
+    });
 }
 
 setup();
+})();
